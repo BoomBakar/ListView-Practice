d05b968df18cb8b89415b3facdf2de78a3c2dd88
BoomBakar <abubakardar608@gmail.com>
2022-05-31 09:16:07 +0500

    List view of student added

--- app/src/main/java/com/example/lec_5/MyAdapter.java
+++ app/src/main/java/com/example/lec_5/MyAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -22,6 +24,19 @@ public class MyAdapter extends ArrayAdapter<Student> {
         Student student = getItem(position);
 
         convertView = LayoutInflater.from(getContext()).inflate(R.layout.student,parent,false);
+
+        TextView name = convertView.findViewById(R.id.name);
+        name.setText(student.getName());
+
+        TextView camp = convertView.findViewById(R.id.campus);
+        camp.setText(student.getSection());
+
+        TextView i = convertView.findViewById(R.id.stdid);
+        i.setText(student.getId());
+
+        ImageView im = convertView.findViewById(R.id.imageView);
+        im.setImageResource(student.getImgId());
+
 
         return convertView;
 
